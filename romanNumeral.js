5aaa50941ce5cac83bd3dbbe660e99458e617fef
Dylan <dylan@dylans-air.home>
2018-07-27 10:24:50 +0100

    toString returns the number in Roman Numerals

--- romanNumeral.js
+++ romanNumeral.js
@@ -1,4 +1,19 @@
 module.exports = function(num) {
+    const RomanNumeralMap = {
+        'M'  : 1000,
+        'CM' : 900,
+        'D'  : 500,
+        'CD' : 400,
+        'C'  : 100,
+        'XC' : 90,
+        'L'  : 50,
+        'XL' : 40,
+        'X'  : 10,
+        'IX' : 9,
+        'V'  : 5,
+        'IV' : 4,
+        'I'  : 1
+    };
     if(num === undefined || null) {
         throw new Error('value required');
     }
@@ -8,6 +23,16 @@ module.exports = function(num) {
         if(+num < 1 || +num > 3999) {
             throw new Error('invalid range');
         }
+        let results = '';
+        let numCopy = num;
+
+        Object.entries(RomanNumeralMap).forEach(([key, value]) => {
+            while(numCopy%value < numCopy) {
+                results += key;
+                numCopy -= value;
+            }
+        });
+        return results;
     }
     return ({
         toInt: toInt,
--- test/test.js
+++ test/test.js
@@ -18,5 +18,37 @@ describe('Roman numeral converter', () => {
             const romanNumeral = new RomanNumeral(4000);
             assert.throws(()=> { romanNumeral.toString() }, "invalid range" );
         });
+        it('1 as value should return I', () => {
+            const romanNumeral = new RomanNumeral(1);
+            assert.equal(romanNumeral.toString(), 'I')
+        });
+        it('3 as value should return III', () => {
+            const romanNumeral = new RomanNumeral(3);
+            assert.equal(romanNumeral.toString(), 'III')
+        });
+        it('4 as value should return IV', () => {
+            const romanNumeral = new RomanNumeral(4);
+            assert.equal(romanNumeral.toString(), 'IV')
+        });
+        it('5 as value should return V', () => {
+            const romanNumeral = new RomanNumeral(5);
+            assert.equal(romanNumeral.toString(), 'V')
+        });
+        it('1968 as value should return MCMLXVIII', () => {
+            const romanNumeral = new RomanNumeral(1968);
+            assert.equal(romanNumeral.toString(), 'MCMLXVIII')
+        });
+        it('1473 as value should return MCDLXXIII', () => {
+            const romanNumeral = new RomanNumeral(1968);
+            assert.equal(romanNumeral.toString(), 'MCMLXVIII')
+        });
+        it('2999 as value should return MMCMXCIX', () => {
+            const romanNumeral = new RomanNumeral(2999);
+            assert.equal(romanNumeral.toString(), 'MMCMXCIX')
+        });
+        it('3000 as value should return MMM', () => {
+            const romanNumeral = new RomanNumeral(3000);
+            assert.equal(romanNumeral.toString(), 'MMM')
+        });
     });
 });
