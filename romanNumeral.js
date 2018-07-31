ea94c63cb690d625e6b32e906f94f3a6ee543b4e
Dylan <dylan@dylans-air.home>
2018-07-31 14:41:49 +0100

    Re-write to string to use recursion

--- romanNumeral.js
+++ romanNumeral.js
@@ -3,54 +3,30 @@ class RomanNumeral {
         if(val === undefined || val === null || val === '') {
             throw new Error('value required');
         }
-        this.RomanNumeralMap = {
-            'M'  : 1000,
-            'CM' : 900,
-            'D'  : 500,
-            'CD' : 400,
-            'C'  : 100,
-            'XC' : 90,
-            'L'  : 50,
-            'XL' : 40,
-            'X'  : 10,
-            'IX' : 9,
-            'V'  : 5,
-            'IV' : 4,
-            'I'  : 1
-        };
+        this.romanNumerals = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+        this.romanNumeralValues = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
         this.val = val;
     }
 
     toInt() {
-        let results = 0;
-        let valToUpperCase = this.val.toUpperCase();
-
-        Object.entries(this.RomanNumeralMap).forEach(([key, value]) => {
-            while(valToUpperCase.indexOf(key)===0) {
-                results += value;
-                valToUpperCase = valToUpperCase.slice(key.length, valToUpperCase.length);
-            }
-        });
-        if(valToUpperCase.length > 0) {
-            throw new Error('invalid value');
-        }
-        return results;
     }
 
     toString() {
         if(+this.val < 1 || +this.val > 3999) {
             throw new Error('invalid range');
         }
-        let results = '';
-        let valCopy = this.val;
+        return this.convertToString(this.romanNumeralValues, this.romanNumerals, this.val);
+    }
 
-        Object.entries(this.RomanNumeralMap).forEach(([key, value]) => {
-            while(valCopy%value < valCopy) {
-                results += key;
-                valCopy -= value;
+    convertToString(romanNumeralValues, romanNumerals, numberValue, stringValue='') {
+        if(numberValue < 1) return stringValue;
+
+        for(let i=0; i<romanNumeralValues.length; i++) {
+            if(numberValue%romanNumeralValues[i] < numberValue) {
+                const newNumberValue = (numberValue -= romanNumeralValues[i]);
+                return this.convertToString(romanNumeralValues, romanNumerals, newNumberValue, stringValue.concat(romanNumerals[i]));
             }
-        });
-        return results;
+        }
     }
 };
 
