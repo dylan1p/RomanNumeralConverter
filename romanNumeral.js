105fd839df44940c794d133599d976a13c8b5752
Dylan <dylan@dylans-air.home>
2018-07-31 13:43:28 +0100

    Convert to using class syntax

--- romanNumeral.js
+++ romanNumeral.js
@@ -1,29 +1,31 @@
-module.exports = function(val) {
-    const RomanNumeralMap = {
-        'M'  : 1000,
-        'CM' : 900,
-        'D'  : 500,
-        'CD' : 400,
-        'C'  : 100,
-        'XC' : 90,
-        'L'  : 50,
-        'XL' : 40,
-        'X'  : 10,
-        'IX' : 9,
-        'V'  : 5,
-        'IV' : 4,
-        'I'  : 1
-    };
-
-    if(val === undefined || val === null || val === '') {
-        throw new Error('value required');
+class RomanNumeral {
+    constructor(val) {
+        if(val === undefined || val === null || val === '') {
+            throw new Error('value required');
+        }
+        this.RomanNumeralMap = {
+            'M'  : 1000,
+            'CM' : 900,
+            'D'  : 500,
+            'CD' : 400,
+            'C'  : 100,
+            'XC' : 90,
+            'L'  : 50,
+            'XL' : 40,
+            'X'  : 10,
+            'IX' : 9,
+            'V'  : 5,
+            'IV' : 4,
+            'I'  : 1
+        };
+        this.val = val;
     }
 
-    function toInt() {
+    toInt() {
         let results = 0;
-        let valToUpperCase = val.toUpperCase();
+        let valToUpperCase = this.val.toUpperCase();
 
-        Object.entries(RomanNumeralMap).forEach(([key, value]) => {
+        Object.entries(this.RomanNumeralMap).forEach(([key, value]) => {
             while(valToUpperCase.indexOf(key)===0) {
                 results += value;
                 valToUpperCase = valToUpperCase.slice(key.length, valToUpperCase.length);
@@ -35,14 +37,14 @@ module.exports = function(val) {
         return results;
     }
 
-    function toString() {
-        if(+val < 1 || +val > 3999) {
+    toString() {
+        if(+this.val < 1 || +this.val > 3999) {
             throw new Error('invalid range');
         }
         let results = '';
-        let valCopy = val;
+        let valCopy = this.val;
 
-        Object.entries(RomanNumeralMap).forEach(([key, value]) => {
+        Object.entries(this.RomanNumeralMap).forEach(([key, value]) => {
             while(valCopy%value < valCopy) {
                 results += key;
                 valCopy -= value;
@@ -50,9 +52,6 @@ module.exports = function(val) {
         });
         return results;
     }
-
-    return ({
-        toInt: toInt,
-        toString: toString
-    });
 };
+
+module.exports = RomanNumeral;
